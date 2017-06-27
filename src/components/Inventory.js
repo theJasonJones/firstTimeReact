41859a6aa4b99b1faaa856c802d5782a4eed0f36
Jason Jones <jkjmr6@gmail.com>
2017-06-27 15:03:15 -0500

    Git config test

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,19 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+	renderInventory( key ){
+		return (
+			<div className="fish-edit" key={key}>
+				
+			</div>
+		);
+	}
+
 	render(){
 		return(
 			<div>
 				<h2>Inventory</h2>
+				{ Object.keys(this.props.fishes).map( this.renderInventory ) }
 				<AddFishForm addFish={this.props.addFish} />
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
