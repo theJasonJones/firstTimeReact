eea0c524be35c6bfa69f25c80645d14f094cfdd4
Jason Jones <jkjmr6@gmail.com>
2017-07-06 12:03:20 -0500

    Add animations to the order component

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAqqD9EV8gjS6I5lFExAVMDzRoD7BUrIWU",
+    authDomain: "catch-of-the-day-99b2c.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-99b2c.firebaseio.com"
+});
+
+export default base;
